1ee744a9efd527ce670c7257521e3e8e80531a8d
caesarwenli <caesarwenli@gmail.com>
2023-03-27 22:39:18 -0600

    pusing

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import User from "App/Models/User";
 import { schema, rules } from "@ioc:Adonis/Core/Validator";
+import Ws from "App/Services/Ws";
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
@@ -14,6 +15,8 @@ export default class UsersController {
     });
     const data = await request.validate({ schema: userSchema });
     const user = await User.create(data);
+
+    Ws.io.emit("new:pet", user);
     return response.status(201).json(user);
   }
 
@@ -74,6 +77,8 @@ export default class UsersController {
       return response.status(404).json({ message: "User not found" });
     }
     await user.delete();
+
+    Ws.io.emit("delete:pet", user);
     return response.status(200).json({ message: "User deleted" });
   }
 }
