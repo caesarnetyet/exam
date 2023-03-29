33c2b68ec894712f10107d545df7155cfe86a8fc
caesarnetyet <caesarnetyet@gmail.com>
2023-03-29 09:33:39 -0600

    arreglo de botes

--- start/socket.ts
+++ start/socket.ts
@@ -3,19 +3,31 @@ import Ws from "App/Services/Ws";
 Ws.boot();
 
 const connectedUsers: string[] = [];
+const boats: string[] = []
 /**
  * Listen for incoming socket connections
  */
 Ws.io.on("connection", (socket) => {
   connectedUsers.push(socket.id);
+  
 
   socket.on("disconnect", () => {
     connectedUsers.splice(connectedUsers.indexOf(socket.id), 1);
+    boats.splice(boats.indexOf(socket.id), 1);
     Ws.io.emit("connectedUsers", connectedUsers);
-    console.log(connectedUsers);
   });
 
-  console.log(connectedUsers);
+  console.log("Connected users", connectedUsers.length);
+
+  socket.on('myPosition', (position:number) => {
+    console.log(position)
+    if (boats.indexOf(socket.id) !== -1) {
+      boats.splice(boats.indexOf(socket.id), 1);
+    }
+    boats[position] = socket.id
+    console.log(boats)
+  })
+  
 
   Ws.io.emit("connectedUsers", connectedUsers);
 
